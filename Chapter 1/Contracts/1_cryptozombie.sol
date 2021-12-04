be7f9aa4303be87983afe57a04b0d98eb4651c17
Kaylah Gore <5260333-kaylahgore@users.noreply.gitlab.com>
2021-12-03 21:23:42 -0600

    Adding Zombie struct with name and dna data

--- Chapter 1/Contracts/1_cryptozombie.sol
+++ Chapter 1/Contracts/1_cryptozombie.sol
@@ -2,6 +2,10 @@ pragma solidity >=0.5.0 < 0.8.10;
 
 contract ZombieFactory {
     uint dnaDigits = 16;
-
     uint dnaModulus = 10 ** dnaDigits;
+    
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
