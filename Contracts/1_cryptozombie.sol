20c6fec85daed15f297f38f7584fbb46ab4bd05f
Kaylah Gore <5260333-kaylahgore@users.noreply.gitlab.com>
2021-12-05 17:15:35 -0600

    updating zombie contract

--- Contracts/1_cryptozombie.sol
+++ Contracts/1_cryptozombie.sol
@@ -14,10 +14,16 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    // mapping to keep track of the address that owns a Zombie
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
-    }
+    } 
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
